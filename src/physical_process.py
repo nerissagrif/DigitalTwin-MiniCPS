55063bbdf74793f3554532bc449330616095337e
nerissagrif <86630903+nerissagrif@users.noreply.github.com>
2024-12-07 18:02:36 -0500

    Update physical_process.py

--- src/physical_process.py
+++ src/physical_process.py
@@ -53,7 +53,7 @@ class LiquidTank(Tank):
             new_level = water_volume / self.section
 
             # update internal and state water level
-            print("DEBUG phys-proc: new_wattage %.5f kW \t delta (volume): %.5f W" % (
+            print("DEBUG phys-proc: new_wattage %.5f W \t delta (volume): %.5f W" % (
                 new_level, (new_level - self.level) * self.section)
             self.level = self.set(SENSOR1, new_level))
 
