879556097269319c5a899ccad9b9ba3619d466b0
nerissagrif <86630903+nerissagrif@users.noreply.github.com>
2024-12-08 21:30:20 -0500

    Update physical_process.py

--- src/physical_process.py
+++ src/physical_process.py
@@ -53,24 +53,24 @@ class LiquidTank(Tank):
             new_level = water_volume / self.section
 
             # update internal and state water level
-            print "DEBUG phys-proc: new_level  %.5f m \t delta (volume): %.5f m3" % (
-                new_level, (new_level - self.level) * self.section)
+            print("DEBUG phys-proc: new_level  %.5f m \t delta (volume): %.5f m3" % (
+                new_level, (new_level - self.level) * self.section))
             self.level = self.set(SENSOR1, new_level)
 
             if new_level <= TANK_M['LowerBound']:
-                print 'DEBUG phys-proc: Tank below lowerbound threshold ', TANK_M['LowerBound']
+                print('DEBUG phys-proc: Tank below lowerbound threshold ', TANK_M['LowerBound'])
                 # break
                 # simulates refill of tank
                 time.sleep(PP_PERIOD_SEC*10)    # simulate time to refill the tank
                 self.level = self.set(SENSOR1, TANK_INIT_LEVEL)
-                print 'DEBUG phys-proc: Tank has been refilled'
+                print('DEBUG phys-proc: Tank has been refilled')
 
             # count += 1
             time.sleep(PP_PERIOD_SEC)
 
     def _stop(self):
 
-        print "physical process stopped (TANK)"
+        print("physical process stopped (TANK)")
 
 
 if __name__ == '__main__':
